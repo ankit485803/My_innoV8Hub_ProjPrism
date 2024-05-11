63237c138dabd50e4d86a9d5d2516332448bacd5
Ankit Kumar <ankit485803@gmail.com>
2024-05-11 16:54:14 +0530

    02_Prof_BackendMastery :  fix bugs register controller and test to upload on cloudinary and Mongodb online through Postman req

--- 02_Profess_BackendMastery_Proj/src/models/user.models.js
+++ 02_Profess_BackendMastery_Proj/src/models/user.models.js
@@ -108,7 +108,7 @@ userSchema.methods.generateRefreshToken = function () {
 
 
 
-export const User = mongoose.models("User", userSchema)
+export const User = mongoose.model("User", userSchema);
 
 
 
