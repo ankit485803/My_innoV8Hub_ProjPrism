8b5b39c65144c291389a03a97e902fa80b341e44
Ankit Kumar <ankit485803@gmail.com>
2026-04-16 00:06:10 +0530

    added the stop venv command

--- 05_myFastAPI_Journey/command.sh
+++ 05_myFastAPI_Journey/command.sh
@@ -13,3 +13,8 @@ pip freeze > requirements.txt
 
 # to run the app server
 uvicorn main:app --reload
+
+
+# stop venv
+(venv) PS C:\Users\sanja\Desktop\GitProj\My_innoV8Hub_ProjPrism\05_myFastAPI_Journey> deactivate
+PS C:\Users\sanja\Desktop\GitProj\My_innoV8Hub_ProjPrism\05_myFastAPI_Journey>
